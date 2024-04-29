db4a4c85d410675301120f5ffc6b32f6444879c9
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-04-29 14:37:28 +0500

    add the legend to it

--- general-force-network/src/components/forcenetworkGraph/LegendData.jsx
+++ general-force-network/src/components/forcenetworkGraph/LegendData.jsx
@@ -6,24 +6,25 @@ const LegendData = ({ uniqueClasses, uniqueClasses2 }) => {
   return (
     <>
       <div className='legend'>
-        <h1 className='text-xl ml-1 font-bold'>Legend data</h1>
-        <ul className='ml-1'>
-          <li className='entity_1'>entity 1 class</li>
-          {uniqueClasses.map((entity_1_li, index) => (
-            <li key={index} >
-            {entity_1_li}
-            </li>
-          ))}
-        </ul>
-        <ul>
-          <li className='entity_2'>entity 2 class</li>
-          {uniqueClasses2.map((entity_2_li, index) => (
-            <li key={index} className='ml-6' >
-            {entity_2_li}
-            </li>
-          ))}
-        </ul>
-      </div>
+  <h1 className='text-xl ml-1 mb-2 font-bold'>Legend data</h1>
+  <ul className='ml-1'>
+    <li className='entity_1 font-semibold'> Entity 1 class</li>
+    {uniqueClasses.map((entity_1_li, index) => (
+      <li key={index }  className='ml-6'>
+        <span className="circle"></span> {entity_1_li}
+      </li>
+    ))}
+  </ul>
+  <ul className='ml-1' >
+    <li className='entity_2 font-semibold'> Entity 2 class</li>
+    {uniqueClasses2.map((entity_2_li, index) => (
+      <li key={index} className='ml-6'>
+        <span className="circle"></span> {entity_2_li}
+      </li>
+    ))}
+  </ul>
+</div>
+
     </>
   );
 };
